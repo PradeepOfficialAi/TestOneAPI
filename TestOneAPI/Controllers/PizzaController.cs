7f2b0be55380550da4b79692dcc07683c803b4f6
PradeepDrive <88648255+PradeepDrive@users.noreply.github.com>
2021-09-23 16:04:41 +0530

    Add project files.

---
+++ TestOneAPI/Controllers/PizzaController.cs
@@ -0,0 +1,68 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using TestOneAPI.Models;
+using TestOneAPI.Services;
+
+namespace TestOneAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class PizzaController : ControllerBase
+    {
+        public PizzaController()
+        {
+
+        }
+
+
+        [HttpGet]
+        public List<Pizzas> Get() => PizzaServices.GetAll();
+
+        [HttpGet("{id}")]
+        public Pizzas GetOne(int id)
+        {
+            var pizza = PizzaServices.Get(id);
+
+            if (pizza == null)
+            {
+                var pizzaRe = new Pizzas();
+                return pizzaRe;
+            }
+            return pizza;
+        }
+
+        [HttpPost] 
+        public IActionResult Create(Pizzas pizza)
+        {
+            PizzaServices.Add(pizza);
+
+            return CreatedAtAction(nameof(Create), new { id = pizza.Id }, pizza);
+        }
+
+        [HttpPut("{id}")] 
+        public IActionResult Update(int id, Pizzas pizza)
+        {
+            if (id != pizza.Id) return BadRequest();
+
+            var inPizza = PizzaServices.Get(id);
+
+            if (inPizza is null)
+                return NotFound();
+            PizzaServices.Update(pizza);
+
+            return NoContent();
+        }
+
+        [HttpDelete("{id}")]
+        public IActionResult Delete(int id)
+        {
+            PizzaServices.Delete(id);
+            return NoContent();
+        }
+
+    }
+}
---
+++ TestOneAPI/Services/PizzaServices.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using TestOneAPI.Models;
+
+namespace TestOneAPI.Services
+{
+    public class PizzaServices
+    {
+        static List<Pizzas> Pizza { get; }
+        static int nextId = 3;
+        static PizzaServices()
+        {
+            Pizza = new List<Pizzas>
+            {
+                new Pizzas()
+                {
+                    Id = 1,
+                    Name = "Classic Italian",
+                    IsGlutenFree = false
+                },
+                new Pizzas()
+                {
+                    Id = 2,
+                    Name = "Veggie",
+                    IsGlutenFree = true
+                }
+            };
+        }
+
+        public static List<Pizzas> GetAll() => Pizza;
+
+        public static Pizzas Get(int id) => Pizza.FirstOrDefault(value => value.Id == id);
+
+        public static void Add(Pizzas pizzas)
+        {
+            pizzas.Id = nextId++;
+            Pizza.Add(pizzas);
+        }
+
+        public static void Update(Pizzas pizza)
+        {
+            var index = Pizza.FindIndex(p => p.Id == pizza.Id);
+            if (index == -1)
+                return;
+
+            Pizza[index] = pizza;
+        }
+
+        public static void Delete(int id)
+        {
+            var pizza = Get(id);
+            if (pizza is null)
+                return;
+
+            Pizza.Remove(pizza);
+        }
+    }
+}
